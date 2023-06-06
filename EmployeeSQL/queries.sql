c3aca91f2f8be939d999d995889721dcf1af6709
aarschne <118419071+aarschne@users.noreply.github.com>
2023-06-05 19:31:21 -0500

    Added first name to salary piece

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,5 +1,5 @@
 ---- Find employee number, last name, sex, and salary of every employee
-SELECT employees.emp_no, employees.last_name, employees.sex, salaries.salary
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 	INNER JOIN salaries 
 		ON employees.emp_no = salaries.emp_no
